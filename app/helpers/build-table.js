a09ac46c9b08821026988a70d97673a86960855b
Tom Fischer <tgfischer6@gmail.com>
2015-03-03 13:01:52 -0500

    Working on GroupBy

--- app/helpers/build-table.js
+++ app/helpers/build-table.js
@@ -1,52 +1,94 @@
 import Ember from 'ember';
 
 export default Ember.Handlebars.makeBoundHelper(function(type, bidOrders, sellOrders) {
-	bidOrders.sortBy('price');
-	sellOrders.sortBy('price');
-
-	var size = bidOrders.length > sellOrders.length ? bidOrders.length : sellOrders.length;
+	bidOrders = bidOrders.sortBy('price');
+	sellOrders = sellOrders.sortBy('price');
 
 	var rows = '';
 
-	for (var i = 0; i < size && i < 10; i++) {
-		switch (type) {
-			case 'order':
+	if (type === 'price') {
+		
+
+		console.log(buyOrderGroup);
+	}
+
+	
+	switch (type) {
+		case 'order':
+			var size = bidOrders.length > sellOrders.length ? bidOrders.length : sellOrders.length;
+
+			for (var i = 0; i < size && i < 10; i++) {
 				rows += '<tr>';
 
 				if (i < bidOrders.length) {
-					rows += '<td class="vertical-align">' + bidOrders.objectAt(i).get('volume') + '</td><td class="vertical-align">$' + bidOrders.objectAt(i).get('price') + '</td>';
+					rows += '<td class="vertical-align">' + bidOrders.objectAt(i).get('volume') + 
+							'</td><td class="vertical-align">$' + bidOrders.objectAt(i).get('price') + '</td>';
 				} else {
 					rows += '<td></td><td></td>';
 				}
 
 				if (i < sellOrders.length) {
-					rows += '<td class="vertical-align">' + sellOrders.objectAt(i).get('volume') + '</td><td class="vertical-align">$' + sellOrders.objectAt(i).get('price') + '</td>';
+					rows += '<td class="vertical-align">' + sellOrders.objectAt(i).get('volume') + 
+							'</td><td class="vertical-align">$' + sellOrders.objectAt(i).get('price') + '</td>';
 				} else {
 					rows += '<td></td><td></td>';
 				}
 
 				rows += '</tr>';
-				break;
-			case 'price':
+			}
+
+			break;
+		case 'price':
+			var buyOrderGroup = [];
+			var sellOrderGroup = [];
+
+			for (var i = 0; i < bidOrders.length; i++) {
+				var order = bidOrders.objectAt(i);
+				var groupedOrder = buyOrderGroup.findBy('price', order.get('price'));
+
+				if (!groupedOrder) {
+					buyOrderGroup.pushObject(Ember.Object.create({
+						num: 1,
+						volume: order.get('volume'),
+						price: order.get('price')
+					}));
+				} else {
+					groupedOrder.set('volume', parseInt(groupedOrder.get('volume')) + parseInt(order.get('volume')));
+					groupedOrder.set('num', parseInt(groupedOrder.get('num') + 1));
+				}
+			}
+
+			for (var i = 0; i < sellOrders.length; i++) {
+
+			}
+
+			var size = buyOrderGroup.length > sellOrderGroup.length ? buyOrderGroup.length : sellOrderGroup.length;
+
+			for (var i = 0; i < size && i < 10; i++) {
 				rows += '<tr>';
 
-				if (i < bidOrders.length) {
-					rows += '<td class="vertical-align">1</td><td class="vertical-align">' + bidOrders.objectAt(i).get('volume') + '</td><td class="vertical-align">$' + bidOrders.objectAt(i).get('price') + '</td>';
+				if (i < buyOrderGroup.length) {
+					rows += '<td class="vertical-align">1</td>' + 
+							'<td class="vertical-align">' + buyOrderGroup.objectAt(i).get('volume') + 
+							'</td><td class="vertical-align">$' + buyOrderGroup.objectAt(i).get('price') + '</td>';
 				} else {
 					rows += '<td></td><td></td><td></td>';
 				}
 
-				if (i < sellOrders.length) {
-					rows += '<td class="vertical-align">1</td><td class="vertical-align">' + sellOrders.objectAt(i).get('volume') + '</td><td class="vertical-align">$' + sellOrders.objectAt(i).get('price') + '</td>';
+				if (i < sellOrderGroup.length) {
+					rows += '<td class="vertical-align">1</td>' +
+							'<td class="vertical-align">' + sellOrderGroup.objectAt(i).get('volume') + 
+							'</td><td class="vertical-align">$' + sellOrderGroup.objectAt(i).get('price') + '</td>';
 				} else {
 					rows += '<td></td><td></td><td></td>';
 				}
 
 				rows += '</tr>';
-				break;
-			default:
-				break;
-		}
+			}
+
+			break;
+		default:
+			break;
 	}
 
 	return new Ember.Handlebars.SafeString(rows);
