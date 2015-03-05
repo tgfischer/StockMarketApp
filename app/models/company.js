88b11576d7a17f2d7c7fd49fe507f64c75bcfc1e
Tom Fischer <tgfischer6@gmail.com>
2015-03-05 11:58:00 -0500

    Moved fixtures into separate files

--- app/models/company.js
+++ app/models/company.js
@@ -1,89 +1,32 @@
 import DS from 'ember-data';
 
-var Company = DS.Model.extend({
+export default DS.Model.extend({
     name           : DS.attr('string'),
-	openPrice      : DS.attr('number'),
-	currentPrice   : DS.attr('number'),
-	volume         : DS.attr('number'),
-	logoURL        : DS.attr('string'),
-	bidOrders      : DS.hasMany('bidorder'),
+    openPrice      : DS.attr('number'),
+    currentPrice   : DS.attr('number'),
+    volume         : DS.attr('number'),
+    logoURL        : DS.attr('string'),
+    bidOrders      : DS.hasMany('bidorder'),
     sellOrders     : DS.hasMany('sellorder'),
 
     // Change this
-	value: function() {
+    value: function() {
         var value = this.get('currentPrice') - this.get('openPrice');
 
-    	if (value === 0) {
-    		return value;
-    	} else {
-    		return value.toFixed(2);
-    	}
+        if (value === 0) {
+            return value;
+        } else {
+            return value.toFixed(2);
+        }
     }.property('openPrice', 'currentPrice'),
 
-	percent: function() {
-    	var percent = Math.abs((this.get('currentPrice') / this.get('openPrice') - 1) * 100);
+    percent: function() {
+        var percent = Math.abs((this.get('currentPrice') / this.get('openPrice') - 1) * 100);
 
-		if (percent === 0) {
-			return  percent;
-		} else {
-			return percent.toFixed(2);
-		}
-	}.property('openPrice', 'currentPrice')
-});
-
-Company.reopenClass({
-	FIXTURES: [
-        {
-            id           : 1,
-            name         : "Microsoft Corporation (MSFT)",
-            openPrice    : 42.59,
-            currentPrice : 42.59,
-            volume       : 0,
-            logoURL      : 'assets/images/microsoft.png',
-            bidOrders    : [],
-            sellOrders   : []
-        },
-        {
-            id           : 2,
-            name         : "Apple Inc. (AAPL)",
-            openPrice    : 121.62,
-            currentPrice : 121.62,
-            volume       : 0,
-            logoURL      : 'assets/images/apple.png',
-            bidOrders    : [],
-            sellOrders   : []
-        },
-        {
-            id           : 3,
-            name         : "Facebook, Inc. (FB)",
-            openPrice    : 74.98,
-            currentPrice : 74.98,
-            volume       : 0,
-            logoURL      : 'assets/images/facebook.png',
-            bidOrders    : [],
-            sellOrders   : []
-        },
-        {
-            id           : 4,
-            name         : "Cisco Systems, Inc.",
-            openPrice    : 27.41,
-            currentPrice : 27.41,
-            volume       : 0,
-            logoURL      : 'assets/images/cisco.png',
-            bidOrders    : [],
-            sellOrders   : []
-        },
-        {
-            id           : 5,
-            name         : "Intel Corporation",
-            openPrice    : 0.55,
-            currentPrice : 0.55,
-            volume       : 0,
-            logoURL      : 'assets/images/intel.png',
-            bidOrders    : [],
-            sellOrders   : []
+        if (percent === 0) {
+            return  percent;
+        } else {
+            return percent.toFixed(2);
         }
-    ]
+    }.property('openPrice', 'currentPrice')
 });
-
-export default Company;
